c96a86bcf6414f34c44b298a813753543a80bea4
Lucasvegaa <lucasvegaa@gmail.com>
2022-11-06 18:45:36 -0300

    fix links NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {Link} from 'react-router-dom';
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="home">
+          <Navbar.Brand href="/home">
             <img
               src="../../Logotipo_Blanco.png"
               width="100"
@@ -17,8 +18,8 @@ function ColorSchemesExample() {
             />
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="profesional">Profesional</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/profesional">Profesional</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
